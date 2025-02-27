aa890ae41835fef1b30a9d2d5a8c9a53e0122501
Dorian Smith <Smith,h.dorian@gmail.com>
2025-02-27 12:56:13 -0600

    initializing repo files, opening for  continuing work on contents

---
+++ js/script.js
@@ -0,0 +1,13 @@
+localStorage.getItem("theme");
+
+localStorage.setItem("theme", newTheme);
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+const systemSettingLight = window.matchMedia("(prefers-color-scheme: light)");
+
+const toggleTheme = () => {
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute("data-theme");
+    root.setAttribute("data-theme", currentTheme === "dark" ? "light" : "dark");
+};
+
+document.querySelector("#theme-toggle").addEventListener("click", toggleTheme);
